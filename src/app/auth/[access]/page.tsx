c900b4cd7672d750bc1f475277445e253f2a66e1
dfunani <dfunani@live.co.za>
2024-11-27 10:13:29 +0200

    Saving Changes - Registration Page

--- src/app/auth/[access]/page.tsx
+++ src/app/auth/[access]/page.tsx
@@ -1,12 +1,10 @@
 // "use client"
 
-import { useParams } from "next/navigation";
 import Login from "./login";
 import Register from "./register";
 import "@/src/app/globals.css"
 
 export default async function Authentication({params}) {
-  // let params = await useParams();
   console.log(await params)
   
   return <div className="main">{(await params).access === "login" ? <Login/> : <Register/>}</div>
--- src/app/auth/[access]/register.tsx
+++ src/app/auth/[access]/register.tsx
@@ -9,108 +9,155 @@ type Regsitration = {
   name: string;
   surname: string;
   dob: string;
-  location: string;
+  city: string;
+  country: string;
 };
 export function Register() {
   const router = useRouter();
+  const [submit, setSubmit] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>("null");
   const [registration, setRegistration] = useState<Regsitration>({
-    username: "",
+    email: "",
     password: "",
+    name: "",
+    surname: "",
     dob: "",
-    location: "",
+    city: "",
+    country: "",
   });
 
+  function handleSubmit() {
+    if (registration.email && registration.password) setSubmit(true);
+    else setError("Please Provide Valid Email and Password.");
+  }
+
   function handleUpdateRegistration(key: string, value: string) {
-    setRegistration((prevRegistration) => {
+    setRegistration((prevRegistration: Regsitration) => {
       return { ...prevRegistration, [key]: value };
     });
   }
-  async function handleSubmit() {
+  async function handleRegistration() {
     let response = await fetch("/api/auth", {
       method: "POST",
-      body: JSON.stringify({
-        username: "usernames",
-        password: "password1",
-        dob: "dob",
-        location: "locations",
-      }),
+      body: JSON.stringify(registration),
     });
     if (response.ok) {
       router.push("/");
     } else router.push("/auth/register");
   }
   return (
-    <div className={styles.main}>
-      <div className={styles["login-container"]}>
-        <h2 className={styles.h2}>Register</h2>
-        <input
-          className={styles.input}
-          aria-label="Name"
-          type="text"
-          placeholder="Name"
-          onChange={(event: ChangeEvent) =>
-            handleUpdateRegistration(
-              "username",
-              (event.target as HTMLInputElement).value
-            )
-          }
-        />
-        <input
-          className={styles.input}
-          aria-label="Surname"
-          type="text"
-          placeholder="Surname"
-          onChange={(event: ChangeEvent) =>
-            handleUpdateRegistration(
-              "username",
-              (event.target as HTMLInputElement).value
-            )
-          }
-        />
-        <input
-          className={styles.input}
-          aria-label="Password"
-          type="password"
-          placeholder="Password"
-          onChange={(event: ChangeEvent) =>
-            handleUpdateRegistration(
-              "username",
-              (event.target as HTMLInputElement).value
-            )
-          }
-        />
-        <input
-          className={styles.input}
-          aria-label="dob"
-          type="date"
-          placeholder="Date of Birth"
-          onChange={(event: ChangeEvent) =>
-            handleUpdateRegistration(
-              "username",
-              (event.target as HTMLInputElement).value
-            )
-          }
-        />
-        <input
-          className={styles.input}
-          aria-label="Location"
-          type="text"
-          placeholder="City, County"
-          onChange={(event: ChangeEvent) =>
-            handleUpdateRegistration(
-              "username",
-              (event.target as HTMLInputElement).value
-            )
-          }
-        />
-        <button
-          className={styles.button}
-          onClick={() => handleSubmit()}
-          type="button"
-        >
-          Submit
-        </button>
-      </div>
+    <div className={styles["login-container"]}>
+      <h2 className={styles.h2}>Register</h2>
+      {!submit && (
+        <div className={styles["form"]}>
+          <input
+            className={styles.input}
+            aria-label="Email"
+            type="email"
+            placeholder="Email"
+            value={registration.email}
+            onChange={(event: ChangeEvent) =>
+              handleUpdateRegistration(
+                "email",
+                (event.currentTarget as HTMLInputElement).value
+              )
+            }
+          />
+          <input
+            className={styles.input}
+            aria-label="Password"
+            type="password"
+            placeholder="Password"
+            value={registration.password}
+            onChange={(event: ChangeEvent) =>
+              handleUpdateRegistration(
+                "password",
+                (event.currentTarget as HTMLInputElement).value
+              )
+            }
+          />
+          <button
+            className={styles.button}
+            onClick={() => handleSubmit()}
+            type="button"
+          >
+            Submit
+          </button>
+        </div>
+      )}
+      <p>{error}</p>
+
+      {submit && (
+        <div className={styles["form"]}>
+          <input
+            className={styles.input}
+            aria-label="Name"
+            type="name"
+            placeholder="Name"
+            onChange={(event: ChangeEvent) =>
+              handleUpdateRegistration(
+                "name",
+                (event.target as HTMLInputElement).value
+              )
+            }
+          />
+          <input
+            className={styles.input}
+            aria-label="Surname"
+            type="text"
+            placeholder="Surname"
+            onChange={(event: ChangeEvent) =>
+              handleUpdateRegistration(
+                "surname",
+                (event.target as HTMLInputElement).value
+              )
+            }
+          />
+          <input
+            className={styles.input}
+            aria-label="Date of Birth"
+            type="date"
+            placeholder="Date of Birth"
+            onChange={(event: ChangeEvent) =>
+              handleUpdateRegistration(
+                "dob",
+                (event.target as HTMLInputElement).value
+              )
+            }
+          />
+          <input
+            className={styles.input}
+            aria-label="City"
+            type="text"
+            placeholder="City"
+            onChange={(event: ChangeEvent) =>
+              handleUpdateRegistration(
+                "city",
+                (event.target as HTMLInputElement).value
+              )
+            }
+          />
+          <input
+            className={styles.input}
+            aria-label="Country"
+            type="text"
+            placeholder="Country"
+            onChange={(event: ChangeEvent) =>
+              handleUpdateRegistration(
+                "country",
+                (event.target as HTMLInputElement).value
+              )
+            }
+          />
+          <button
+            className={styles.button}
+            onClick={() => handleRegistration()}
+            type="button"
+          >
+            Submit
+          </button>
+        </div>
+      )}
     </div>
   );
 }
